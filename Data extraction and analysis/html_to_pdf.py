7f246d01d315b725e2efc3a18648783cbb7479ee
saliveja <esman@riseup.net>
2022-05-18 14:40:34 +0200

    fixed formatting of pdf

--- Data extraction and analysis/html_to_pdf.py
+++ Data extraction and analysis/html_to_pdf.py
@@ -1,14 +1,15 @@
 # import wkhtmltopdf
-from bs4 import BeautifulSoup
+import requests, bs4
 from urllib.request import Request, urlopen
 from datetime import date, datetime, timedelta
 import feedparser
 import requests
-import weasyprint
+import pdfkit
+# from django.http import HttpResponse
 
-#
-# sources = {'Hayes': 'https://cryptohayes.medium.com/',
-#            'Su Zhu & Hasu': 'https://uncommoncore.co/',
+
+sources = ['https://cryptohayes.medium.com/feed',
+           'https://uncommoncore.co/feed/']
 #            'Our network': 'https://ournetwork.substack.com/',
 #            'Messari': 'https://messari.io/research',
 #            '0xparc': 'https://0xparc.org/blog',
@@ -19,18 +20,7 @@ import weasyprint
 #            'Metagovernance project': 'https://metagov.org/',
 #            'Bitmex': 'https://blog.bitmex.com/?lang=en_us',
 # }
-url = 'https://cryptohayes.medium.com/'
-feed = feedparser.parse("https://cryptohayes.medium.com/feed")
-link_list = []
-
-for entry in feed.entries:
-    # print(entry.published)
-    for item in entry.link:
-        links = entry.link
-        link_list.append(links)
-
-document = link_list[0]
-# index 0 is the latest article
+# url = 'https://cryptohayes.medium.com/'
 
 today_date = date.today()
 # print(today_date)
@@ -45,12 +35,24 @@ three = today_date + timedelta(days=-3)
 two = today_date + timedelta(days=-2)
 one = today_date + timedelta(days=-1)
 
-for entry in feed.entries:
-    if entry.published in url == today_date or one or two or three or four \
-            or five or six or seven:
-        name = entry.title
-        name_edited = name.title()
-        weasyprint.HTML(document).write_pdf(f"{name.title()}.pdf")
-        # wkhtmltopdf(f"url={document}, output_file={name_edited}.pdf")
-    else:
-        print("sorry, nothing new.")
+link_list = []
+
+for thing in sources:
+    feed = feedparser.parse(thing)
+    for entry in feed.entries:
+        # print(entry.published)
+        for item in entry.link:
+            links = entry.link
+            link_list.append(links)
+
+        document = link_list[0]
+        # index 0 is the latest article
+
+        for item in entry.published:
+            if item == today_date or one or two or three or four \
+                    or five or six or seven:
+                name = entry.title
+                # read_text = entry.content
+                pdfkit.from_url(document, f"{name}.pdf")
+            else:
+                print("There are no new articles published.")
