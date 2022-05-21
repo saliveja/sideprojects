736f39111a58dbf2b3f2ba0bf102a8bed79201c9
saliveja <esman@riseup.net>
2022-05-21 13:28:04 +0200

    making dictionary of links and process communication

--- Data extraction and analysis/html_to_pdf.py
+++ Data extraction and analysis/html_to_pdf.py
@@ -1,15 +1,13 @@
-# import wkhtmltopdf
 import requests, bs4
 from urllib.request import Request, urlopen
 from datetime import date, datetime, timedelta
 import feedparser
 import requests
 import pdfkit
-# from django.http import HttpResponse
 
 
-sources = ['https://cryptohayes.medium.com/feed',
-           'https://uncommoncore.co/feed/']
+url_feed = {'hayes':'https://cryptohayes.medium.com/feed',
+           'uncommoncore':'https://uncommoncore.co/feed',}
 #            'Our network': 'https://ournetwork.substack.com/',
 #            'Messari': 'https://messari.io/research',
 #            '0xparc': 'https://0xparc.org/blog',
@@ -35,24 +33,46 @@ three = today_date + timedelta(days=-3)
 two = today_date + timedelta(days=-2)
 one = today_date + timedelta(days=-1)
 
-link_list = []
+url_hayes = 'https://cryptohayes.medium.com/feed'
+links = {}
 
-for thing in sources:
-    feed = feedparser.parse(thing)
-    for entry in feed.entries:
-        # print(entry.published)
-        for item in entry.link:
-            links = entry.link
-            link_list.append(links)
+feed = feedparser.parse(url_hayes)
+# print(feed.entries)
 
-        document = link_list[0]
-        # index 0 is the latest article
+for entry in feed.entries:
+    if entry.published == today_date or one or two or three or four \
+                        or five or six or seven:
+        name = entry.title
+        links[name] = entry.link
+    else:
+            print("There are no new articles published.")
 
-        for item in entry.published:
-            if item == today_date or one or two or three or four \
-                    or five or six or seven:
-                name = entry.title
-                # read_text = entry.content
-                pdfkit.from_url(document, f"{name}.pdf")
-            else:
-                print("There are no new articles published.")
+
+for name, link in links.items():
+    print("Creating PDF ", name)
+    pdfkit.from_url(link, f"{name}.pdf")
+    print(f"Created PDF {name} successfully!")
+
+# url_ucc = 'https://uncommoncore.co/blog/feed'
+#
+# link_list_ucc = []
+#
+# feed = feedparser.parse(url_ucc)
+# for entry in feed.entries:
+#     # print(entry.published)
+#     # print(entry.link)
+#     link_list_ucc.append(entry.link)
+#
+#     documentUcc = link_list_ucc[0]
+#     # index 0 is the latest article
+#     print(documentUcc)
+#
+#     for dates in entry.published
+#         print(dates)
+#         if dates == today_date or one or two or three or four \
+#                 or five or six or seven:
+#             name = entry.title
+#             # read_text = entry.content
+#             pdfkit.from_url(documentUcc, f"{name}.pdf")
+#         else:
+#             print("There are no new articles published.")
