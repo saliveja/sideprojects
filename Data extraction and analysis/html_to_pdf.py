cb3751cbd78b11bfbe4c0e709043753bb0200df6
saliveja <esman@riseup.net>
2022-05-22 17:54:28 +0200

    working with timedelta

--- Data extraction and analysis/html_to_pdf.py
+++ Data extraction and analysis/html_to_pdf.py
@@ -4,7 +4,6 @@ from datetime import date, datetime, timedelta
 import feedparser
 import requests
 import pdfkit
-from dateutil.parser import parse
 import time
 import calendar
 
@@ -20,22 +19,26 @@ url_feed = {'hayes':'https://cryptohayes.medium.com/feed',
 #            'Metagovernance project': 'https://metagov.org/',
 #            'Bitmex': 'https://blog.bitmex.com/?lang=en_us',
 # }
-# url = 'https://cryptohayes.medium.com/'
 
 today_date = date.today()
-# print(today_date)
 # printing the date of today
-month = timedelta(days=30)
 
-#
-# seven = today_date + timedelta(days=-7)
-# # displaying the date seven days before today's date
-# six = today_date + timedelta(days=-6)
-# five = today_date + timedelta(days=-5)
-# four = today_date + timedelta(days=-4)
-# three = today_date + timedelta(days=-3)
-# two = today_date + timedelta(days=-2)
-# one = today_date + timedelta(days=-1)
+fifteen = today_date - timedelta(days=15)
+fourteen = today_date - timedelta(days=14)
+thirteen = today_date - timedelta(days=13)
+twelve = today_date - timedelta(days=12)
+eleven = today_date - timedelta(days=11)
+ten = today_date - timedelta(days=10)
+nine = today_date - timedelta(days=9)
+eight = today_date - timedelta(days=8)
+seven = today_date - timedelta(days=7)
+# displaying the date seven days before today's date
+six = today_date - timedelta(days=6)
+five = today_date - timedelta(days=5)
+four = today_date - timedelta(days=4)
+three = today_date - timedelta(days=3)
+two = today_date - timedelta(days=2)
+one = today_date - timedelta(days=1)
 
 url_hayes = 'https://cryptohayes.medium.com/feed'
 links = {}
@@ -46,25 +49,34 @@ feed = feedparser.parse(url_hayes)
 numDays = 30
 date_list = []
 date_rss = {}
-
-for x in range(numDays):
-    date_list.append(today_date - timedelta(days = x))
-    # appending all past 30 days to date_list
+#
+# for x in range(numDays):
+#     date_list.append(today_date - timedelta(days = x))
+#     # appending all past 30 days to date_list
 
 for entry in feed.entries:
         name = entry.title
         links[name] = entry.link
         # key is name, value is the value in entry.link
-        print(entry.published)
+        # print(entry.published)
         date_rss[name] = date.fromtimestamp(calendar.timegm(time.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z")))
-# print(links)
+print(links)
 print(date_rss)
-print(date_list)
-for name, link in links.items():
-    if date_rss[name] < today_date - timedelta(days=30):
-        print(f'{name} published {date_rss[name]}, checking < {today_date - timedelta(days=30)}')
+# print(today_date)
+
+dates = today_date - timedelta(days=30)
+print(dates)
+#
+for name, date in date_rss.items():
+    if date_rss[name] == today_date or one or two or three or four or \
+            five or six or seven or eight or nine or ten or eleven or twelve \
+            or thirteen or fourteen or fifteen:
+        print(f'{name} published {date_rss[name]}, checking < {today_date}, '
+              f'{one}, {two}, {three}, {four}, {five}, {six}, {seven}, {eight},'
+              f'{nine}, {ten}, {eleven}, {twelve}, {thirteen}, {fourteen}, '
+              f'{fifteen}')
         print(f"Creating PDF , {name}")
-        pdfkit.from_url(link, f"{name}.pdf")
+        pdfkit.from_url(links[name], f"{name}.pdf")
         print(f"Created PDF {name} successfully!")
     else:
         print(f"{date_rss}")
