52eb848929a1715e51765bba3effbb9e188b35d8
saliveja <esman@riseup.net>
2022-07-05 21:38:18 +0200

    lopping through href and parsing p

--- Data extraction and analysis/articles/summary_newsletters.py
+++ Data extraction and analysis/articles/summary_newsletters.py
@@ -7,6 +7,10 @@ import feedparser
 import pdfkit
 import requests, bs4
 import nltk
+import re
+from nltk.tokenize import LineTokenizer
+from nltk.tokenize.stanford_segmenter import StanfordSegmenter
+import numpy as np
 
 # url = 'https://entrepreneurshandbook.co/number-three-511f334d8fae'
 # article = Article(url)
@@ -40,15 +44,11 @@ def summarize(text, per):
     select_length=int(len(sentence_tokens)*per)
     summary=nlargest(select_length, sentence_scores,key=sentence_scores.get)
     final_summary=[word.text for word in summary]
-    summary=' '.join(final_summary)
+    summary=' .'.join(final_summary)
     return summary
 
 
-def save_article_to_file():
-
-    article = 'article.txt'
-    with open(article, 'w') as f:
-        f.truncate()
+def parsing_html_saving_text_to_file():
 
     links = []
     urls = {
@@ -62,44 +62,55 @@ def save_article_to_file():
         res.raise_for_status()
         soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-        for article in soup.find_all('a'):
-            link = article.get('href')
-            links.append(link)
+        if (each_code := soup.find_all('a')[9]):
+            x = each_code.get('href')
+            links.append(x)
 
-    req1 = requests.get(links[9], headers={'User-Agent': 'Mozilla/5.0'})
-    soup1 = bs4.BeautifulSoup(req1.text, 'html.parser')
-    html = soup1.find_all('p')
+    for link in links:
+        req1 = requests.get(link, headers={'User-Agent': 'Mozilla/5.0'})
+        soup1 = bs4.BeautifulSoup(req1.text, 'html.parser')
+        html = soup1.find_all('p')
+        print(link)
+        print(html)
 
-    for text in html:
-        article = 'article.txt'
-        with open(article, 'a+') as f:
-            text_str = str(text.text)
-            f.write(text_str)
-    links.clear()
+        for text in html:
+            article = 'article.txt'
+            with open(article, 'w+') as f:
+                text_str = str(text.text)
+                f.write(text_str)
 
-    for key, value in urls.items():
-        if key == "Knower's substack":
-            bullet_points = []
-            article_file = open('article.txt', "r+")
-            article_new = open("article_new.txt", "w")
-
-            tags = soup1.find_all("li")
-            for content in tags:
-                # nltk.download('punkt')
-                text = content.text
-                string_to_list_items = nltk.tokenize.sent_tokenize(text)[0]
-                bullet_points.append(string_to_list_items)
-
-            for article in article_file:
-                for line in bullet_points:
-                    line_new_2 = f" {line}."
-                    article = article.replace(line, line_new_2)
-
-                article_new.write(article)
+    #
+    # article_file = open('article.txt', "r+")
+    # article_new = open("article_new.txt", "w")
+    #
+    # req1 = requests.get(links[9], headers={'User-Agent': 'Mozilla/5.0'})
+    # soup1 = bs4.BeautifulSoup(req1.text, 'html.parser')
+    # html = soup1.find_all('p')
+    #
+    # bullet_points = []
+    # tags = soup1.find_all("li")
+    # for key, value in urls.items():
+    #     res = requests.get(urls[key], headers={'User-Agent': 'Mozilla/5.0'})
+    #     res.raise_for_status()
+    #     soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    #
+    #     for content in tags:
+    #         if tags in soup:
+    #         # nltk.download('punkt')
+    #             text = content.text
+    #             string_to_list_items = nltk.tokenize.sent_tokenize(text)[0]
+    #             bullet_points.append(string_to_list_items)
+    #
+    #         for article in article_file:
+    #             for line in bullet_points:
+    #                 line_new_2 = f" {line}."
+    #                 article = article.replace(line, line_new_2)
+    #
+    #             article_new.write(article)
 
 def save_summary():
     """Summarizing the article and saving to a file"""
-    file = 'article_new.txt'
+    file = 'article.txt'
     with open(file, 'r') as fx:
         text_to_sum = fx.read()
         file_sum = 'summary.txt'
@@ -107,6 +118,11 @@ def save_summary():
             sum = summarize(text_to_sum, 0.05)
             fs.write(sum)
             fs.write("\n\n")
+
+def truncate():
+    article = 'article.txt'
+    with open(article, 'w') as f:
+        f.truncate()
 
 
 # def knower():
@@ -171,8 +187,9 @@ def save_summary():
 #             fs.write("\n\n")
 #
 # knower()
-save_article_to_file()
-save_summary()
+parsing_html_saving_text_to_file()
+# save_summary()
+# truncate()
 
 
 
